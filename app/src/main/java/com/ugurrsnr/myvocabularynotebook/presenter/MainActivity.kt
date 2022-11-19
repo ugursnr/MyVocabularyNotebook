a3353a568d3ad7ef5266ad272f1112b4a0b32d31
Ugur <ugurr.snr@gmail.com>
2022-11-19 13:26:31 +0300

    Initial commit

---
+++ app/src/main/java/com/ugurrsnr/myvocabularynotebook/presenter/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.ugurrsnr.myvocabularynotebook.presenter
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.ugurrsnr.myvocabularynotebook.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}

16d9ef058f6ec0fde5b0198992ea1ee7b50da00c
Ugur <ugurr.snr@gmail.com>
2022-11-29 00:02:33 +0300

    Design.

---
+++ app/src/main/java/com/ugurrsnr/myvocabularynotebook/presenter/view/QuizFragment.kt
@@ -0,0 +1,25 @@
+package com.ugurrsnr.myvocabularynotebook.presenter.view
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.ugurrsnr.myvocabularynotebook.R
+import com.ugurrsnr.myvocabularynotebook.databinding.FragmentQuizBinding
+
+class QuizFragment : Fragment() {
+
+    private var _binding : FragmentQuizBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentQuizBinding.inflate(layoutInflater,container, false)
+        return binding.root
+    }
+
+
+}
